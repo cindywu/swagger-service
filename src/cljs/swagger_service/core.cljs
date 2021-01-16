c6c42ec3220b31158df1d8a6d36bf9e950d4382b
cindy <ciindy.wu@gmail.com>
2021-01-16 13:30:48 -1000

    Paginate the cats

--- src/cljs/swagger_service/core.cljs
+++ src/cljs/swagger_service/core.cljs
@@ -17,14 +17,43 @@
         ^{:key link}
         [:div.col-sm-4 [:img {:width 400 :src link}]])])])
 
+(defn forward [i pages]
+  (if (< i (dec pages)) (inc i) i))
+
+(defn back [i]
+  (if (pos? i) (dec i) i))
+
+(defn nav-link [page i]
+  [:li.page-item>a.page-link.btn.btn-primary
+   {:on-click #(reset! page i)
+    :class (when (= i @page) "active")}
+   [:span i]])
+
+(defn pager [pages page]
+  (when (> pages 1)
+    (into
+     [:div.text-xs-center>ul.pagination.pagination-lg]
+     (concat
+      [[:li.page-item>a.page-link.btn
+        {:on-click #(swap! page back pages)
+         :class (when (= @page 0) "disabled")}
+        [:span "«"]]]
+      (map (partial nav-link page) (range pages))
+      [[:li.page-item>a.page-link.btn
+        {:on-click #(swap! page forward pages)
+         :class (when (= @page (dec pages)) "disabled")}
+        [:span "»"]]]))))
+
 (defn home-page []
-  (let [links (atom nil)
-        page (atom 0)]
-    (fetch-links! links 20)
+  (let [page (atom 0)
+        links (atom nil)]
+    (fetch-links! links 50)
     (fn []
-      [:div
-       (when @links
-         [images (@links @page)])])))
+      (if (not-empty @links)
+        [:div.container>div.row>div.col-md-12
+         [pager (count @links) page]
+         [images (@links @page)]]
+        [:div "Standby for cats!"]))))
 
 (defn mount-components []
   (reagent/render-component [home-page] (.getElementById js/document "app")))
