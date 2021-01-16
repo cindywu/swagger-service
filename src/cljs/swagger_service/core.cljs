dfc371612b17b7a0ba772c0700d85dc5d9e3f887
cindy <ciindy.wu@gmail.com>
2021-01-16 13:03:24 -1000

    Fetches 20 cats

--- src/cljs/swagger_service/core.cljs
+++ src/cljs/swagger_service/core.cljs
@@ -3,14 +3,15 @@
             [ajax.core :refer [GET]])
   (:require-macros [secretary.core :refer [defroute]]))
 
+(defn fetch-links! [links link-count]
+  (GET "/api/cat-links"
+    {:params {:link-count link-count}
+     :handler #(reset! links %)}))
+
 (defn home-page []
   (let [links
-        (atom
-         ["http://25.media.tumblr.com/Jjkybd3nSafemf3rYocB7QcC_500.jpg"
-          "http://25.media.tumblr.com/tumblr_ln4zdhp4Uj1qcnzavo1_500.gif"
-          "http://24.media.tumblr.com/tumblr_m2kmg2VK2a1qhwmnpo1_1280.jpg"
-          "http://24.media.tumblr.com/tumblr_m30w1mNl1w1qgjltdo1_1280.jpg"
-          "http://25.media.tumblr.com/tumblr_m3gm5oqm9e1r73wdao1_500.jpg"])]
+        (atom nil)]
+    (fetch-links! links 20)
     (fn []
       [:div
        (for [link @links]
