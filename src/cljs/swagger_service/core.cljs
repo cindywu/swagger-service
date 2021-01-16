d3c70206f0be9cd842c3eab8bc6b493a7c8c52f9
cindy <ciindy.wu@gmail.com>
2021-01-16 12:57:54 -1000

    Show 5 cats on home-page

--- src/cljs/swagger_service/core.cljs
+++ src/cljs/swagger_service/core.cljs
@@ -4,8 +4,17 @@
   (:require-macros [secretary.core :refer [defroute]]))
 
 (defn home-page []
-  [:div
-   [:h1 "TODO: show some cats..."]])
+  (let [links
+        (atom
+         ["http://25.media.tumblr.com/Jjkybd3nSafemf3rYocB7QcC_500.jpg"
+          "http://25.media.tumblr.com/tumblr_ln4zdhp4Uj1qcnzavo1_500.gif"
+          "http://24.media.tumblr.com/tumblr_m2kmg2VK2a1qhwmnpo1_1280.jpg"
+          "http://24.media.tumblr.com/tumblr_m30w1mNl1w1qgjltdo1_1280.jpg"
+          "http://25.media.tumblr.com/tumblr_m3gm5oqm9e1r73wdao1_500.jpg"])]
+    (fn []
+      [:div
+       (for [link @links]
+         [:img {:src link}])])))
 
 (defn mount-components []
   (reagent/render-component [home-page] (.getElementById js/document "app")))
